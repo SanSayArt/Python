d5aa2c3073c79e01c6a5a49fd8fb792042e90e0d
SanSayArt <SanSayArt@gmail.com>
2023-12-24 19:14:42 +0500

    rename

---
+++ Sem3/Task_17.py
@@ -0,0 +1,23 @@
+"""
+Задача №17. Решение в группах
+Дан список чисел. Определите, сколько в нем
+встречается различных чисел.
+Input: [1, 1, 2, 0, -1, 3, 4, 4]
+Output: 6
+
+"""
+
+arr = [1, 1, 2, 0, -1, 3, 4, 4]
+
+print(len(set(arr)))
+
+# Традиционный итератор с функцией APPEND
+lst = []
+for el in arr:
+    if el not in lst:
+        lst.append(el)
+print(len(lst))
+
+# Списковое включение (lc)
+# lst = [el for el in arr if el not in lst]
+# print(len(lst))
